9e0a052de9d238d80b80341219ee5341a2960c1c
kowal <pkowalski91@gmail.com>
2016-01-25 21:04:48 +0100

    first

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            // startuje licznie
+            Example();
+            // to co wpisales
+            string result = Console.ReadLine();
+            Console.WriteLine("napisales: " + result);
+        }
+    }
+
+    static async void Example()
+    {
+        // ta metoda dziala asynchronicznie
+        int t = await Task.Run(() => Allocate());
+        Console.WriteLine("obliczam: " + t);
+    }
+
+    static int Allocate()
+    {
+        // obliczam calkowita ilost liczb w stringu
+        int size = 0;
+        for (int z = 0; z < 100; z++)
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                string value = i.ToString();
+                if (value == null)
+                {
+                    return 0;
+                }
+                size += value.Length;
+            }
+        }
+        return size;
+    }
+}
